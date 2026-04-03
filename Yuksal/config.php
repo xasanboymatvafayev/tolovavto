913606179ea6c1ab350e5fadca0b9bf72e828732
Xasanboy <xasanboymatvapayev@icloud.com>
2026-04-03 14:26:13 +0500

    Update config.php

--- Yuksal/config.php
+++ Yuksal/config.php
@@ -1,16 +1,16 @@
 <?php
-date_Default_timezone_set('Asia/Tashkent');
-define('API_KEY','8248107');
+date_default_timezone_set('Asia/Tashkent');
+
+define('API_KEY', getenv('TELEGRAM_BOT_TOKEN'));
 
 $sana = date("d.m.Y");
 $soat = date("H:i");
 
-define("DB_SERVER", "localhost"); 
-define("DB_USERNAME", ""); 
-define("DB_PASSWORD", ''); 
-define("DB_NAME", ""); 
+define("DB_SERVER",   getenv('DB_HOST'));
+define("DB_USERNAME", getenv('DB_USER'));
+define("DB_PASSWORD", getenv('DB_PASS'));
+define("DB_NAME",     getenv('DB_NAME'));
 
 $connect = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-mysqli_set_charset($connect,"utf8mb4");
-
+mysqli_set_charset($connect, "utf8mb4");
 ?>
