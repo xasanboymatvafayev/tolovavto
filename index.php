91847bf65018585475b9740b74a5c12b09264317
Xasanboy <xasanboymatvapayev@icloud.com>
2026-04-05 20:09:37 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
+$request = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+
+if ($request === 'pay' || $request === 'pay.php') {
+    require __DIR__ . '/pay.php';
+    exit;
+}
+
+if ($request === 'docs' || $request === 'docs.html') {
+    readfile(__DIR__ . '/docs.html');
+    exit;
+}
+
 header("Location: /sub/");
 exit;
